452756e3ed6d4d9dddab59a80b05347104e97893
airtoxin <airtoxin@icloud.com>
2019-11-16 06:55:34 +0900

    Add tests

---
+++ src/sudoku.test.ts
@@ -0,0 +1,84 @@
+import "jest";
+import { solve } from "./sudoku";
+
+describe("sudoku", () => {
+  it("1x1 minimal", () => {
+    const solutions = solve(1, []);
+
+    let sol = solutions.next();
+    expect(sol.value).toEqual([[1]]);
+
+    sol = solutions.next();
+    expect(sol.done).toBe(true);
+  });
+
+  it("4x4", () => {
+    const puzzle = [
+      { x: 1, y: 1, num: 4 },
+      { x: 3, y: 1, num: 3 },
+      { x: 2, y: 2, num: 3 },
+      { x: 3, y: 3, num: 2 },
+      { x: 2, y: 4, num: 2 },
+      { x: 4, y: 4, num: 3 }
+    ];
+    const solutions = solve(4, puzzle);
+    expect(solutions.next().done).toEqual(false);
+  });
+
+  it("9x9", () => {
+    const puzzle = [
+      {x: 1, y: 1, num: 9},
+      {x: 3, y: 1, num: 6},
+      {x: 4, y: 1, num: 7},
+      {x: 6, y: 1, num: 5},
+      {x: 7, y: 1, num: 4},
+      {x: 9, y: 1, num: 2},
+      {x: 4, y: 2, num: 6},
+      {x: 5, y: 2, num: 9},
+      {x: 6, y: 2, num: 4},
+      {x: 1, y: 3, num: 4},
+      {x: 3, y: 3, num: 7},
+      {x: 5, y: 3, num: 3},
+      {x: 7, y: 3, num: 5},
+      {x: 9, y: 3, num: 9},
+      {x: 1, y: 4, num: 2},
+      {x: 2, y: 4, num: 5},
+      {x: 4, y: 4, num: 3},
+      {x: 5, y: 4, num: 7},
+      {x: 6, y: 4, num: 1},
+      {x: 8, y: 4, num: 8},
+      {x: 9, y: 4, num: 6},
+      {x: 2, y: 5, num: 7},
+      {x: 3, y: 5, num: 3},
+      {x: 4, y: 5, num: 5},
+      {x: 5, y: 5, num: 6},
+      {x: 6, y: 5, num: 9},
+      {x: 7, y: 5, num: 1},
+      {x: 8, y: 5, num: 2},
+      {x: 1, y: 6, num: 1},
+      {x: 2, y: 6, num: 6},
+      {x: 4, y: 6, num: 4},
+      {x: 5, y: 6, num: 8},
+      {x: 6, y: 6, num: 2},
+      {x: 8, y: 6, num: 5},
+      {x: 9, y: 6, num: 7},
+      {x: 1, y: 7, num: 7},
+      {x: 3, y: 7, num: 1},
+      {x: 5, y: 7, num: 2},
+      {x: 7, y: 7, num: 6},
+      {x: 9, y: 7, num: 5},
+      {x: 4, y: 8, num: 1},
+      {x: 5, y: 8, num: 4},
+      {x: 6, y: 8, num: 6},
+      {x: 1, y: 9, num: 6},
+      {x: 3, y: 9, num: 8},
+      {x: 4, y: 9, num: 9},
+      {x: 6, y: 9, num: 7},
+      {x: 7, y: 9, num: 2},
+      {x: 9, y: 9, num: 1}
+    ];
+    const solutions = solve(9, puzzle);
+    expect(solutions.next().done).toEqual(false);
+    expect(solutions.next().done).toEqual(true);
+  });
+});
--- src/sudoku.ts
+++ src/sudoku.ts
@@ -1,142 +1,62 @@
 import * as Logic from "logic-solver";
 import { iterateSolution, range as _range } from "./helpers";
 
-const range = (size: number) => _range(size, 1);
+export interface Cell {
+  x: number;
+  y: number;
+  num: number;
+}
 
-const size = 9;
-const boxSize = Math.sqrt(size);
-
-let cnt = 0;
-const solver = new Logic.Solver();
-const getCellVariable = (x: number, y: number, z: number) => {
-  cnt++;
-  return `C${x}-${y}:${z}`;
-};
+const getCellVariable = (cell: Cell) => `C${cell.x}-${cell.y}:${cell.num}`;
 const getCellNumber = (cellVariable: string) => Number.parseInt(cellVariable.split(":")[1], 10);
 
-const boardVariables = range(size).map(y => range(size).map(x => range(size).map(num => getCellVariable(x, y, num))));
+export function *solve(size: number, filledCells: Cell[]): IterableIterator<number[][]> {
+  const boxSize = Math.sqrt(size);
+  const solver = new Logic.Solver();
 
-for (const a of range(size)) {
-  for (const b of range(size)) {
-    // num in cell
-    const cellVariables = range(size).map(num => getCellVariable(a, b, num));
-    solver.require(Logic.exactlyOne(...cellVariables));
+  const boardVariables = range(size).map(y => range(size).map(x => range(size).map(num => getCellVariable({x, y, num}))));
 
-    // num in row
-    const rowVariables = range(size).map(x => getCellVariable(x, a, b));
-    solver.require(Logic.exactlyOne(...rowVariables));
+  for (const a of range(size)) {
+    for (const b of range(size)) {
+      // num in cell
+      const cellVariables = range(size).map(num => getCellVariable({ x: a, y: b, num }));
+      solver.require(Logic.exactlyOne(...cellVariables));
 
-    // // num in col
-    const colVariables = range(size).map(y => getCellVariable(a, y, b));
-    solver.require(Logic.exactlyOne(...colVariables));
-  }
-}
-// num in block
-for (const boxY of range(boxSize)) {
-  const boxOffsetY = (boxY - 1) * boxSize;
-  for (const boxX of range(boxSize)) {
-    const boxOffsetX = (boxX - 1) * boxX;
-    const boxCells = range(boxSize).flatMap(y => range(boxSize). flatMap(x => ({ x: boxOffsetX + x, y: boxOffsetY + y })));
+      // num in row
+      const rowVariables = range(size).map(x => getCellVariable({x, y: a, num: b}));
+      solver.require(Logic.exactlyOne(...rowVariables));
 
-    for (const num of range(size)) {
-      solver.require(Logic.exactlyOne(...boxCells.map(({ x, y }) => getCellVariable(x, y, num))));
+      // // num in col
+      const colVariables = range(size).map(y => getCellVariable({x: a, y, num: b}));
+      solver.require(Logic.exactlyOne(...colVariables));
     }
   }
+  // num in block
+  // for (const boxY of range(boxSize)) {
+  //   const boxOffsetY = (boxY - 1) * boxSize;
+  //   for (const boxX of range(boxSize)) {
+  //     const boxOffsetX = (boxX - 1) * boxX;
+  //     const boxCells = range(boxSize).flatMap(y => range(boxSize). flatMap(x => ({ x: boxOffsetX + x, y: boxOffsetY + y })));
+  //
+  //     for (const num of range(size)) {
+  //       solver.require(Logic.exactlyOne(...boxCells.map(({ x, y }) => getCellVariable({x, y, num}))));
+  //     }
+  //   }
+  // }
+
+  for (const filledCell of filledCells) {
+    solver.require(getCellVariable(filledCell));
+  }
+
+  for (const solution of iterateSolution(solver)) {
+    const boardSolution = boardVariables.map(
+      rowVariables => rowVariables.map(
+        variablesInCell => variablesInCell.find(cellVariable => solution.evaluate(cellVariable)) as string
+      ).map(getCellNumber)
+    );
+    solver.forbid(solution.getFormula());
+    yield boardSolution;
+  }
 }
 
-// problem
-// solver.require(
-//   "1-1:9",
-//   "3-1:6",
-//   "4-1:7",
-//   "6-1:5",
-//   "7-1:4",
-//   "9-1:2",
-//   "4-2:6",
-//   "5-2:9",
-//   "6-2:4",
-//   "1-3:4",
-//   "3-3:7",
-//   "5-3:3",
-//   "7-3:5",
-//   "9-3:9",
-//   "1-4:2",
-//   "2-4:5",
-//   "4-4:3",
-//   "5-4:7",
-//   "6-4:1",
-//   "8-4:8",
-//   "9-4:6",
-//   "2-5:7",
-//   "3-5:3",
-//   "4-5:5",
-//   "5-5:6",
-//   "6-5:9",
-//   "7-5:1",
-//   "8-5:2",
-//   "1-6:1",
-//   "2-6:6",
-//   "4-6:4",
-//   "5-6:8",
-//   "6-6:2",
-//   "8-6:5",
-//   "9-6:7",
-//   "1-7:7",
-//   "3-7:1",
-//   "5-7:2",
-//   "7-7:6",
-//   "9-7:5",
-//   "4-8:1",
-//   "5-8:4",
-//   "6-8:6",
-//   "1-9:6",
-//   "3-9:8",
-//   "4-9:9",
-//   "6-9:7",
-//   "7-9:2",
-//   "9-9:1"
-// );
-// solver.require(
-//   "1-1:4",
-//   "3-1:3",
-//   "2-2:3",
-//   "3-3:2",
-//   "2-4:2",
-//   "4-4:3"
-// );
-solver.require(
-  "C3-3:3",
-  "C6-6:3",
-);
-// solver.require(
-//   "1-1:8",
-//   "3-2:3",
-//   "4-2:6",
-//   "2-3:7",
-//   "5-3:9",
-//   "7-3:2",
-//   "2-4:5",
-//   "6-4:7",
-//   "5-5:4",
-//   "6-5:5",
-//   "7-5:7",
-//   "4-6:1",
-//   "8-6:3",
-//   "3-7:1",
-//   "8-7:6",
-//   "9-7:8",
-//   "3-8:8",
-//   "4-8:5",
-//   "8-8:1",
-//   "2-9:9",
-//   "7-9:4"
-// );
-
-for (const solution of iterateSolution(solver)) {
-  const boardSolution = boardVariables.map(
-    rowVariables => rowVariables.map(
-      variablesInCell => variablesInCell.find(cellVariable => solution.evaluate(cellVariable)) as string
-    ).map(getCellNumber)
-  );
-  console.log(boardSolution.map(row => row.join(" ")).join("\n") + "\n\n");
-}
+const range = (size: number) => _range(size, 1);
